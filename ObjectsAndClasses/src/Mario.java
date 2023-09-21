687b0945ef8cf07cef3e5b4a7e95d1439218817e
HASSANI IDRISSI Rim <74383907+harim12@users.noreply.github.com>
2023-09-21 11:06:22 +0100

    objects and classes

---
+++ ObjectsAndClasses/src/Mario.java
@@ -0,0 +1,13 @@
+public class Mario {
+
+    String eyesColor;
+    String hatColor;
+    String costumeColor;
+
+    public Mario(String eyesColor,String hatColor,String costumeColor){
+        this.eyesColor = eyesColor;
+        this.hatColor = hatColor;
+        this.costumeColor = costumeColor;
+    }
+
+}
